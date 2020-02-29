4aabdf8109e99e6614c764388ff2c6ee2cf88ee1
Suzane <suzysoutosilva@gmail.com>
2020-02-29 19:27:19 -0300

    Chat simples com gRPC.

---
+++ client.js
@@ -0,0 +1,56 @@
+let grpc = require("grpc");
+var protoLoader = require("@grpc/proto-loader");
+var readline = require("readline");
+
+//Lendo as linhas do terminal.
+var rl = readline.createInterface({
+    input:process.stdin,
+    output: process.stdout
+});
+
+//Carregando o protobuf
+var proto = grpc.loadPackageDefinition(
+    protoLoader.loadSync("protos/chat.proto", {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
+    })
+);
+
+const REMOTE_SERVER = "0.0.0.0:5001";
+
+let username;
+
+//Criando um client gRPC.
+let client = new proto.example.Chat(
+    REMOTE_SERVER,
+    grpc.credentials.createInsecure()
+);
+
+//Iniando o stream entre servidor e cliente
+function startChat() {
+    let channel = client.join({ user: username});
+
+    channel.on("data", onData);
+
+    rl.on("line", function(text) {
+        client.send({ user: username, text: text }, res => {});
+    });
+}
+
+//Quando o servidor envia a menssagem.
+function onData(message) {
+    if(message.user == username) {
+        return;
+    }
+    console.log(`${message.user}: ${message.text}`);
+}
+
+//Pergunta o nome do usuário e então inicia o chat.
+rl.question("What's ur name? ", answer => {
+    username = answer;
+
+    startChat();
+});
---
+++ server.js
@@ -0,0 +1,41 @@
+let grpc = require("grpc");
+var protoLoader = require("@grpc/proto-loader");
+
+const server = new grpc.Server();
+const SERVER_ADDRESS = "0.0.0.0:5001";
+
+//Carregamento do protobuf
+let proto = grpc.loadPackageDefinition(
+    protoLoader.loadSync("protos/chat.proto", {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
+    })
+);
+
+let users = [];
+
+//Menssagem recebida do cliente quando entra no chat.
+function join(call, callback) {
+    users.push(call);
+    notifyChat({ user: "Server", text: "new user joined ..."});
+}
+
+//Menssagem recebida do cliente.
+function send(call, callback) {
+    notifyChat(call.request);
+}
+
+//Menssagem enviada para todos os clientes conectados.
+function notifyChat(message) {
+    users.forEach(user => {
+        user.write(message);
+    });
+}
+
+//Definindo os métodos e iniciando o servidor.
+server.addService(proto.example.Chat.service, { join: join, send: send });
+server.bind(SERVER_ADDRESS, grpc.ServerCredentials.createInsecure());
+server.start();
